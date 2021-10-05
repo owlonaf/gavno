fe722a7c2eed83b965acecc85309f23de10befb2
imukov <imukov@o-code.ru>
2021-10-05 12:02:53 +0400

    Initial commit

---
+++ src/main/java/com/imukov/bdeck/BdeckApplication.java
@@ -0,0 +1,13 @@
+package com.imukov.bdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imukov/bdeck/BdeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imukov.bdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
